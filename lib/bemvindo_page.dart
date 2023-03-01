89de86a54ff5830c3ab8969d2ec69bcbf97c7056
Igor Cavalcanti <igoraraujocavalcanti@gmail.com>
2023-03-01 18:08:07 -0300

    Tela de Cadastro

--- lib/bemvindo_page.dart
+++ lib/bemvindo_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:alcomt_puro/cadastro_page.dart';
 
 class BemVindoPage extends StatefulWidget {
   const BemVindoPage({Key? key}) : super(key: key);
 
   @override
   State<BemVindoPage> createState() => _BemVindoPageState();
-
 }
 
 class _BemVindoPageState extends State<BemVindoPage> {
@@ -26,48 +26,45 @@ class _BemVindoPageState extends State<BemVindoPage> {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Image.asset('assets/logo_alcomt.png'),
-
                 SizedBox(height: 20),
-
                 TextFormField(
                   controller: _emailController,
                   decoration: InputDecoration(
                     labelText: 'E-mail',
                     hintText: 'nome@email.com',
+                    filled: true, // ativa o background
+                    fillColor: Colors.white.withOpacity(0.2), // define o background da caixa de texto com transparência    
                   ),
-                  validator: (email){
-                    if(email == null || email.isEmpty) {
+                  validator: (email) {
+                    if (email == null || email.isEmpty) {
                       return 'Digite seu email';
                     }
                     return null;
                   },
                 ),
-
                 SizedBox(height: 12),
-
                 TextFormField(
                   controller: _senhaController,
+                  obscureText: true, // mascara a senha com pontos
                   decoration: InputDecoration(
                     labelText: 'Senha',
                     hintText: 'Digite sua senha',
+                    filled: true, // ativa o background
+                    fillColor: Colors.white.withOpacity(0.2), // define o background da caixa de texto com transparência    
                   ),
-                  validator: (senha){
-                    if(senha == null || senha.isEmpty) {
+                  validator: (senha) {
+                    if (senha == null || senha.isEmpty) {
                       return 'Digite sua senha';
                     }
                     return null;
                   },
                 ),
-
                 SizedBox(height: 15),
-
                 ElevatedButton(
-                  onPressed: (){},
+                  onPressed: () {},
                   child: Text(
                     'ENTRAR',
-                    style: TextStyle(
-                        fontSize: 18,
-                      color: Colors.black),
+                    style: TextStyle(fontSize: 18, color: Colors.black),
                   ),
                   style: ButtonStyle(
                     backgroundColor: MaterialStateProperty.all(Colors.white),
@@ -75,50 +72,52 @@ class _BemVindoPageState extends State<BemVindoPage> {
                       EdgeInsets.symmetric(vertical: 15),
                     ),
                     textStyle: MaterialStateProperty.all(
-                      TextStyle(
-                          color: Colors.black,
-                          fontSize: 18),
+                      TextStyle(color: Colors.black, fontSize: 18),
                     ),
                   ),
                 ),
-
                 SizedBox(height: 20),
-
                 Row(
                   children: [
                     Expanded(
-                      child: ElevatedButton(
-                        onPressed: (){},
+                      child: ElevatedButton(  //botão Registre-se
+                        onPressed: () {},
                         child: Text(
-                          'Registre-se',
+                          'Recuperar a Senha',
                           style: TextStyle(
                             color: Colors.black,
                             fontSize: 16,
                           ),
                         ),
                         style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
                           padding: MaterialStateProperty.all(
                             EdgeInsets.symmetric(vertical: 15),
                           ),
                         ),
                       ),
                     ),
-
                     SizedBox(width: 15),
-
                     Expanded(
                       child: ElevatedButton(
-                        onPressed: (){},
+                        onPressed: () {
+                          Navigator.push(   //Navega para a página
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CadastroPage()),
+                          );                        
+                        },
                         child: Text(
-                          'Criar conta',
+                          'Cadastrar',
                           style: TextStyle(
                             color: Colors.black,
                             fontSize: 16,
                           ),
                         ),
                         style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.white),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
                           padding: MaterialStateProperty.all(
                             EdgeInsets.symmetric(vertical: 15),
                           ),
