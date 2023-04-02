474da8d8277ae97efe8e5a73aef51b51b942828c
Igor Fernandes <igormdfernandes@gmail.com>
2023-04-02 16:53:10 -0300

    menu

--- lib/MenuPage.dart
+++ lib/MenuPage.dart
@@ -4,10 +4,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:alcomt_puro/LoginPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:alcomt_puro/adicNotifPage.dart';
+import 'package:alcomt_puro/NotificacaoEspecificaPage.dart';
 
-class MenuPage extends StatelessWidget {
-  const MenuPage({Key? key, required this.auth}) : super(key: key);
+class MenuPage extends StatefulWidget {
   final FirebaseAuth auth;
+  const MenuPage({Key? key, required this.auth}) : super(key: key);
+
+  @override
+  _MenuPageState createState() => _MenuPageState();
+}
+
+class _MenuPageState extends State<MenuPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +32,11 @@ class MenuPage extends StatelessWidget {
         ),
         leading: IconButton(
           onPressed: () async {
-            await auth.signOut(); // Desautentica o usuário
+            await widget.auth.signOut(); // Desautentica o usuário
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => LoginPage(auth: auth)),
+              MaterialPageRoute(
+                  builder: (context) => LoginPage(auth: widget.auth)),
             );
           },
           icon: Icon(
@@ -33,55 +45,110 @@ class MenuPage extends StatelessWidget {
           ),
         ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream:
-            FirebaseFirestore.instance.collection('notificacoes').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context, index) {
-              final notificacao = snapshot.data!.docs[index];
-
-              return GestureDetector(
-                onTap: () {
-                  //Navigator.push(
-                  //context,
-                  //MaterialPageRoute(
-                  //builder: (context) => NotifiEspecificaPage(notificacao: notificacao),
-                  //    ),
-                  //);
-                },
-                child: Card(
-                  child: ListTile(
-                    title: Text(
-                      notificacao['bairro'],
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              constraints: BoxConstraints(
+                  minHeight: 40), // altura mínima definida para o container
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Spacer(),
+                  Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/person-icon.png'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                    subtitle: Text(
-                      DateTime.fromMillisecondsSinceEpoch(
-                              notificacao['data'].millisecondsSinceEpoch)
-                          .toString(),
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.normal,
+                      //botão minha conta
+                      child: Ink(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      MinhaContaPage(auth: widget.auth)),
+                            );
+                          },
+                          child: SizedBox(
+                            width: 80,
+                            height: 40,
+                          ),
+                        ),
+                      )),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/logo_alcomt.png',
+              width: 200,
+              height: 200,
+            ),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('notificacoes')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final notificacao = snapshot.data!.docs[index];
+
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => NotificacaoEspecificaPage(
+                                notificacao: notificacao),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        child: ListTile(
+                          title: Text(
+                            notificacao['bairro'],
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            DateTime.fromMillisecondsSinceEpoch(
+                                    notificacao['data'].millisecondsSinceEpoch)
+                                .toString(),
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          trailing: Icon(Icons.arrow_forward),
+                        ),
                       ),
-                    ),
-                    trailing: Icon(Icons.arrow_forward),
-                  ),
-                ),
-              );
-            },
-          );
-        },
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
       ),
       floatingActionButton: Container(
         alignment: Alignment.bottomCenter,
