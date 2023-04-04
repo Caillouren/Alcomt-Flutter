eba878ef15aa9eaf0fc72af4c5752e828bd4f887
MarcosNeto14 <marcosneto01@hotmail.com>
2023-04-04 20:54:12 -0300

    Melhorias e funcionalidades na notificacao especifica

--- lib/NotificacaoEspecificaPage.dart
+++ lib/NotificacaoEspecificaPage.dart
@@ -4,52 +4,61 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:alcomt_puro/LoginPage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:alcomt_puro/adicNotifPage.dart';
+import 'package:intl/intl.dart';
 
 class NotificacaoEspecificaPage extends StatelessWidget {
-  final DocumentSnapshot notificacao;
-
-  NotificacaoEspecificaPage({required this.notificacao});
+  final QueryDocumentSnapshot<Object?> notificacao;
+  const NotificacaoEspecificaPage({Key? key, required this.notificacao}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Map<String, dynamic> data = notificacao.data() as Map<String, dynamic>;
     return Scaffold(
       appBar: AppBar(
         title: Text('Notificação Específica'),
       ),
-      body: Padding(
+      body: Container(
         padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              notificacao['bairro'],
+              data['bairro'],
               style: TextStyle(
                 fontSize: 24.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
+            SizedBox(height: 8.0),
+            Text(
+              DateFormat('dd/MM/yyyy').format(data['data'].toDate()),
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             SizedBox(height: 16.0),
             Text(
-              DateTime.fromMillisecondsSinceEpoch(
-                      notificacao['data'].millisecondsSinceEpoch)
-                  .toString(),
+              data['descricao'],
               style: TextStyle(
                 fontSize: 18.0,
                 fontWeight: FontWeight.normal,
               ),
             ),
-            SizedBox(height: 18.0),
-            FloatingActionButton(
-              backgroundColor: Colors.black,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          adicNotifPage(auth: FirebaseAuth.instance)),
-                );
-              },
-              child: Icon(Icons.add),
+            SizedBox(height: 16.0),
+            if (data.containsKey('imagem'))
+              Image.network(
+                data['imagem'],
+                width: 200,
+                height: 200,
+              ),
+            SizedBox(height: 16.0),
+            Text(
+              'Tipo: ${data['tipo']}',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.normal,
+              ),
             ),
           ],
         ),
